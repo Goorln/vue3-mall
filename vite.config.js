362bcc1bab99226e18f77540429b2455d18e3a81
Goorln <goorln@163.com>
2023-09-23 11:19:12 +0800

    element主题色色定制

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,13 +16,24 @@ export default defineConfig({
             resolvers: [ElementPlusResolver()],
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
+            // 配置element-plus采用sass样式配色系统
+            resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
         }),
+        // ElementPlus({
+        //     useSource: true,
+        // }),
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src',
                 import.meta.url))
         }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/element/index.scss" as *;`,
+            },
+        },
     }
 })
