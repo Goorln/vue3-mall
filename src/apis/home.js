48e089bd58384cbaa347f2e8a138098ffbd1740c
Goorln <goorln@163.com>
2023-09-25 20:40:59 +0800

    产品列表的实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,19 +1,29 @@
 import httpInstance from "@/utils/http";
 
+// 轮播图接口
 export function getHomeBannerAPI() {
     return httpInstance({
         url: '/home/banner'
     })
 }
 
+// 新鲜好物
 export function getHomeNewAPI() {
     return httpInstance({
         url: '/home/new'
     })
 }
 
+// 人气推荐
 export function getHomeHotAPI() {
     return httpInstance({
         url: '/home/hot'
+    })
+}
+
+// 产品列表
+export function getHomeGoodsAPI() {
+    return httpInstance({
+        url: '/home/goods'
     })
 }
