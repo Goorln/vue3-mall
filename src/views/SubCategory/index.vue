112837038ce73c40a52877421a4eba6f7c4e8301
Goorln <goorln@163.com>
2023-09-26 16:10:50 +0800

    二级分类页面基础列表的渲染实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,16 +1,31 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { getCategoryFilterAPI } from "@/apis/category";
+import { getCategoryFilterAPI, getSubCategoryAPI } from "@/apis/category";
 import { useRoute } from "vue-router";
 import GoodsItem from "@/views/Home/components/GoodsItem.vue";
 const route = useRoute()
+
 //获取面包屑导航数据
-const categoryData = ref({})
+const breadcrumbData = ref({})
 const getCategory = async () => {
   const res = await getCategoryFilterAPI(route.params.id)
-  categoryData.value = res.result
+  breadcrumbData.value = res.result
 }
-onMounted(() => getCategory())
+//获取基础列表数据
+const subCategoryData = ref([])
+const reqParams = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: 'publishTime'
+})
+const getSubCategory = async () => {
+  const res = await getSubCategoryAPI(reqParams.value)
+  console.log(res);
+  subCategoryData.value = res.result.items
+}
+
+onMounted(() => getCategory(), getSubCategory())
 
 </script>
 
@@ -20,9 +35,9 @@ onMounted(() => getCategory())
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}
+        <el-breadcrumb-item :to="{ path: `/category/${breadcrumbData.parentId}` }">{{ breadcrumbData.parentName }}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ breadcrumbData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
@@ -33,7 +48,7 @@ onMounted(() => getCategory())
       </el-tabs>
       <div class="body">
         <!-- 商品列表-->
-        <!-- <GoodsItem :goods="categoryData.goods" /> -->
+        <GoodsItem v-for="good in subCategoryData" :key="good.id" :goods="good" />
       </div>
     </div>
   </div>
