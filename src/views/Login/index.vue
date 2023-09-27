28bfe76a7142193eeb817832ce2bc5428202215a
Goorln <goorln@163.com>
2023-09-27 20:55:32 +0800

    登录页-表单验证

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+// 准备表单对象
+const form = ref({
+  account: '',
+  password: ''
+})
+//准备规则对象
+const rules = {
+  account: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码为6-14个字符', trigger: 'blur' }
+  ]
+}
 
 </script>
 
@@ -8,7 +22,7 @@
     <header class="login-header">
       <div class="container m-top-20">
         <h1 class="logo">
-          <RouterLink to="/">小兔鲜</RouterLink>
+          <RouterLink to="/">樂寕檬兒</RouterLink>
         </h1>
         <RouterLink class="entry" to="/">
           进入网站首页
@@ -24,12 +38,12 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form ref="form" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+              <el-form-item label="账户" prop="account">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
