7f78c9929f86058410bce8895e0ab58e1ddfcc70
Goorln <goorln@163.com>
2023-09-28 10:09:39 +0800

    token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import { useRouter } from 'vue-router'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -24,8 +25,18 @@ httpInstance.interceptors.request.use(config => {
 
 //axios 响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+    // 1. 从pinia里面获取token数据
+    const userStore = useUserStore()
+    const router = useRouter()
     // 统一错误提示
     ElMessage({ type: 'warning', message: e.response.data.msg })
+    // 401 token 失效处理
+    // 清除本地用户数据
+    // 跳转到登录页
+    if (e.response.status === 401) {
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
