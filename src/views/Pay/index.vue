0b0924888fe416b30a510b18ce450d450e3b9aa4
Goorln <goorln@163.com>
2023-10-07 10:22:38 +0800

    支付页面-获取基础的支付信息

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,5 +1,14 @@
 <script setup>
-const payInfo = {}
+import { ref, onMounted } from 'vue'
+import { getOrderAPI } from '@/apis/pay'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const payInfo = ref({})
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.id)
+  payInfo.value = res.result
+}
+onMounted(() => getPayInfo())
 </script>
 
 
