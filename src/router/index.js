6299d7be80317cd8836d7a1a7f7d5e7893ca7a0a
Goorln <goorln@163.com>
2023-10-07 16:23:50 +0800

    会员中心-个人中心组件和订单组件

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,10 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
+
 
 const router = createRouter({
     history: createWebHistory(
@@ -46,6 +50,18 @@ const router = createRouter({
             }, {
                 path: 'paycallback',
                 component: PayBack
+            }, {
+                path: 'member',
+                component: Member,
+                children: [{
+                        path: 'userinfo',
+                        component: UserInfo
+                    },
+                    {
+                        path: 'userorder',
+                        component: UserOrder
+                    }
+                ]
             }
         ]
     }, {
